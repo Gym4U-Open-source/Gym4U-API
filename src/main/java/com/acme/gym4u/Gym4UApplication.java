de5c690f5e13897584db40d351d07d273c83c8f1
Breydi Ramos Terrones <67590524+XxProFetxX@users.noreply.github.com>
2022-11-02 16:23:33 -0500

    chore: upload base project.

---
+++ src/main/java/com/acme/gym4u/Gym4UApplication.java
@@ -0,0 +1,13 @@
+package com.acme.gym4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gym4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gym4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/gym4u/Gym4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.gym4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gym4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
