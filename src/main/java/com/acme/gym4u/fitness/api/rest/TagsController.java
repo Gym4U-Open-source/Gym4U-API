c606742942c256308acdb95216d7f255470d57e0
JuanPabloPT2001 <u201915206@gmail.com>
2022-11-06 19:30:54 -0500

    feat(tags): controller post method implemented

--- src/main/java/com/acme/gym4u/fitness/api/rest/TagsController.java
+++ src/main/java/com/acme/gym4u/fitness/api/rest/TagsController.java
@@ -2,15 +2,18 @@ package com.acme.gym4u.fitness.api.rest;
 
 import com.acme.gym4u.fitness.domain.service.TagService;
 import com.acme.gym4u.fitness.mapping.TagMapper;
+import com.acme.gym4u.fitness.resource.CreateTagResource;
 import com.acme.gym4u.fitness.resource.TagResource;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/tags", produces = "application/json")
@@ -29,6 +32,16 @@ public class TagsController {
     @Operation(summary = "Get all tags")
     public Page<TagResource> getAllTags(Pageable pageable) {
         return mapper.modelListPage(tagService.getAll(), pageable);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create Tag",
+            responses = {@ApiResponse(description = "Tag successfully created", responseCode = "201", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TagResource.class)))})
+    public ResponseEntity<TagResource> createTag(
+            @RequestBody CreateTagResource resource) {
+        return new ResponseEntity<>(mapper.toResource(
+                tagService.create(mapper.toModel(resource))),
+                HttpStatus.CREATED);
     }
 
 }
