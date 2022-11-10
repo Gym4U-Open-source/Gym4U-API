4c209da288480d3276825d60d4d994e5787e6460
Breydi Ramos Terrones <67590524+XxProFetxX@users.noreply.github.com>
2022-11-09 19:59:06 -0500

    feat(profile): profile facade implemented.

---
+++ src/main/java/com/acme/gym4u/profile/service/ProfileFacadeImpl.java
@@ -0,0 +1,18 @@
+package com.acme.gym4u.profile.service;
+
+import com.acme.gym4u.profile.api.internal.ProfileContextFacade;
+import com.acme.gym4u.profile.domain.model.entity.Profile;
+import com.acme.gym4u.profile.domain.service.ProfileService;
+
+public class ProfileFacadeImpl  implements ProfileContextFacade {
+    private final ProfileService profileService;
+
+    public ProfileFacadeImpl(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @Override
+    public Profile create(Profile profile) {
+        return profileService.create(profile);
+    }
+}
