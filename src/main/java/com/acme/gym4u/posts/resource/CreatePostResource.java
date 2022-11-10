b2eef1788dc03564a30ce716c44e57a0d6460fd1
ClinderSGT <u201920785@upc.edu.pe>
2022-11-10 16:05:42 -0500

    Feat: Add post resources

---
+++ src/main/java/com/acme/gym4u/posts/resource/CreatePostResource.java
@@ -0,0 +1,24 @@
+package com.acme.gym4u.posts.resource;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePostResource {
+
+    @NotNull
+    @NotBlank
+    @Size(max = 120)
+    private String title;
+
+    private String description;
+
+    private String urlImage;
+}
