c981e31401ec08727a49ede54ac34e3a94f9befa
JuanPabloPT <89485954+JuanPabloPT@users.noreply.github.com>
2022-11-10 20:52:55 -0500

    feat(workouts): implementing workout entity and tagToWorkout entity

---
+++ src/main/java/com/acme/gym4u/fitness/domain/model/entity/TagToWorkout.java
@@ -0,0 +1,28 @@
+package com.acme.gym4u.fitness.domain.model.entity;
+
+import com.acme.gym4u.shared.domain.model.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tagToWorkout")
+public class TagToWorkout extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    @Column(unique = true)
+    private String name;
+}
---
+++ src/main/java/com/acme/gym4u/fitness/domain/model/entity/Workout.java
@@ -0,0 +1,31 @@
+package com.acme.gym4u.fitness.domain.model.entity;
+
+import com.acme.gym4u.shared.domain.model.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+@Entity
+@Table(name = "workouts")
+public class Workout extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max=50)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tag_id", nullable = true)
+    private TagToWorkout tag;
+}
